2442569e545b44eebe91e6fd9823c7095b1d7ab5
王晋杰 <2310385247@qq.com>
2019-07-23 14:51:55 +0800

    双指针遍历

---
+++ 10-leetcode/1-删除排序数组中的重复项.js
@@ -0,0 +1,17 @@
+// https://github.com/azl397985856/leetcode/blob/master/problems/26.remove-duplicates-from-sorted-array.md
+
+var removeDuplicates = function(nums) {
+    const size = nums.length
+    let slowP = 0
+    for (let fastP = 0; fastP < size; fastP++) {
+        if (nums[fastP] !== nums[slowP]) {
+            slowP++
+            nums[slowP] = nums[fastP]
+        }
+    }
+    return slowP + 1
+}
+
+var nums = [0, 0, 1, 1, 1, 2, 2, 3, 3, 4]
+const res = removeDuplicates(nums)
+console.log(res, nums)
